ed990b86ae9105566c0ecd1d80d134794d68de5c
kostyantyn <funyourmind@gmail.com>
2022-10-28 12:38:29 +0200

    add ec2.tf, main.tf, modify .gitignore, move username.sh into terraform folder

---
+++ terraform/ec2.tf
@@ -0,0 +1,14 @@
+# Create EC2 instance
+resource "aws_instance" "scaling_fastapi_server" {
+  ami           = "ami-0d593311db5abb72b"
+  instance_type = "t3.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.private_subnet_1.id
+  vpc_security_group_ids      = [aws_security_group.http_security.id]
+  key_name                    = "vockey"
+  user_data                   = file("userdata.sh")
+  
+  tags = {
+    Name = "Scaling Fastapi Server"
+  }
+}
---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-west-2"
+}

dc86ab0c5264bd1d71edb46b45b7d6d55faf78c9
kostyantyn <funyourmind@gmail.com>
2022-10-30 11:48:01 +0100

    update gateway_id -> nat_gateway_id in loadbalancer.tf

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -61,7 +61,7 @@ resource "aws_default_route_table" "private_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat.id
   }
 
 
